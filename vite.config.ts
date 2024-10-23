b9152bb4ef962c05b7e1dd9aa91647dcc03b1e60
Safe Gergis <safegerg@gmail.com>
2024-10-23 16:34:17 -0700

    added shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
 })
