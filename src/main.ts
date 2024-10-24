b9623f5eb01a90a4688a6daa9020771e651f453d
Safe Gergis <safegerg@gmail.com>
2024-10-23 21:44:47 -0700

    unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,5 @@ import './assets/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import CanvasJSStockChart from '@canvasjs/vue-stockcharts'
-
 const app = createApp(App)
-app.use(CanvasJSStockChart)
 app.mount('#app')
