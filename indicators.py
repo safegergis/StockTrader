1b4ccdf45499cb3cddb662a6a7a4b8e2bd95ebd5
Joshua Zamora <143128699+Noblebluehalo@users.noreply.github.com>
2024-10-23 20:32:04 -0700

    Update indicators.py
    idk if im doing this right

--- indicators.py
+++ indicators.py
@@ -1,1 +1,9 @@
-
+#data = List prices
+#period = number of days for the moving average
+def calculate_sma(data, period):
+  sma = []
+  for i in range (period -1, len(data));
+    window = data[i - period +1;i +1]
+    sma.append(sum(window) /period
+               return s
+  
